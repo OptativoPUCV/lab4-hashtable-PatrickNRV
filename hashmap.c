d2770ad4f3318221fb17b5bad0695064874c4020
PatrickNRV <162918186+PatrickNRV@users.noreply.github.com>
2024-04-25 16:04:42 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -84,7 +84,12 @@ void enlarge(HashMap * map) {
 
 HashMap * createMap(long capacity) {
 
-    return NULL;
+  HashMap * map = (HashMap *)malloc(sizeof(HashMap));
+  map->buckets = (Pair **)malloc(capacity * sizeof(Pair *));
+  map->size = 0;
+  map->capacity = capacity;
+  
+  return NULL;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
